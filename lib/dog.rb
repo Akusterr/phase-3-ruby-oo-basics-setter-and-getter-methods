7455600159d378192a0f16c0a0e04cc34dcf6257
Alexa <kusteralexa@gmail.com>
2023-01-10 13:42:12 -0500

    commit tree 6853f9c4ad2ae06af6a36082ada2d1fc6d795f68

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,21 @@
-class Dog
+require 'pry'
 
+class Dog
+    def initialize(name, breed, job)
+        @name = name
+        @breed = breed
+    end
+    def name
+        @name
+    end
+    def breed
+        @breed
+    end
+    def job
+        @job
+    end
 end
+
+Torby = Dog.new
+
+binding.pry
